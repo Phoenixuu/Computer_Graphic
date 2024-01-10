c338aaaaa240dcc6ef109f6fb4480cab7e5f0813
unknown <dung2000xd@gmail.com>
2024-01-10 22:08:02 +0700

    move triangle

--- OpenGLCourseApp/OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/OpenGLCourseApp/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
@@ -7,17 +8,24 @@
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VBO, VAO, shader;
+GLuint VBO, VAO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxoffset = 0.7f;
+float triIncrement = 0.0005f;
 
 // Vertex Shader code
 static const char* vShader = "                                                \n\
 #version 330                                                                  \n\
                                                                               \n\
 layout (location = 0) in vec3 pos;											  \n\
+																			  \n\
+uniform float xMove;														  \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);				  \n\
+    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);		  \n\
 }";
 
 // Fragment Shader
@@ -115,6 +123,8 @@ void CompileShaders()
 		return;
 	}
 
+	uniformXMove = glGetUniformLocation(shader, "xMove");
+
 }
 
 int main()
@@ -175,11 +185,24 @@ int main()
 		// Get + Handle user input events
 		glfwPollEvents();
 
+		if (direction) {
+			triOffset += triIncrement;
+		}
+		else {
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxoffset) {
+			direction = !direction;
+		}
+
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+
+		glUniform1f(uniformXMove, triOffset);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
